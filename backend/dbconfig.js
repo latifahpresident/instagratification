06d97a1a8341745e27515f75a617ad96e26582f4
Latifah <latifah.pres@gmail.com>
2020-09-20 22:02:53 -0500

    fixing travis

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -1,4 +1,4 @@
-const knex = require('knex');
+const knex = require("knex");
 const knexConfig = require('./knexfile');
 const environment = process.env.NODE_ENV || 'development'
 const db = knex(knexConfig[environment]);

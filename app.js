eb029ed66256c0fb7445ef77e26b880fe2d204ab
Latifah <latifah.pres@gmail.com>
2021-02-19 12:13:59 -0500

    attempting to fix cors error

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const userRoutes = require("./routes/useraccount");
 const commentRoutes = require("./routes/comment");
 
 app.use(express.json());
-app.use(helmet());
 app.use(cors());
-app.use(function(req, res, next) {
-    // res.header("Access-Control-Allow-Origin", "instaham-5n6obilzz.vercel.app");
-        
-    res.header("Access-Control-Allow-Origin", "*"); //comment out when pushing to production
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+app.use(helmet());
+
+app.use(function(req, res, next) {        
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*') 
+    res.header('Access-Control-Allow-Methods', "GET,OPTIONS,PATCH,DELETE,POST,PUT") 
+    res.header("Access-Control-Allow-Headers", "Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version")
     next();
 });
 app.use("/", authRoutes);
